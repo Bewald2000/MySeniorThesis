43aaaa516f2904293745046f655bd3a3dc1a6e9d
Bewald2000 <Bewald@Carthage.edu>
2024-02-07 12:07:10 -0600

    Update 1
    added validation information to data, and fixing index information.

--- SeniorProject/app/models/user.rb
+++ SeniorProject/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    validates :fname, presence: true
+    validates :lname, presence: true, length: { minimum: 2}
+    validates :prename, presence: null
+    validates :email, presence: true, length: { minimum: 4}
+    validates :phonenumber, presence: true, length { minimum: 14}
 end
